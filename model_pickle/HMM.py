9bf661b917558f9561b9deb1371ebc8c81e9e661
EKUMartin <movieunknown85@gmail.com>
2026-01-20 17:28:04 +0900

    HMM 추가

--- model_pickle/HMM.py
+++ model_pickle/HMM.py
@@ -7,13 +7,25 @@ BASE = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(BASE))
 import data.process_data as pp
 import DB.db_conn as db
-
-
-
 # 20일 MA,  20일의 volatility, 거래량, 종가의 위치((종가-최저가)/(최고가-최저가))
 conn,cur=db.open_db()
-def _20_ma():
-    
-    return ma20
-
-model = GaussianHMM(n_components=4, verbose=True, n_iter=100, random_state= 108).fit(X)
+sql="""
+select Open,High,Low,Close,Volume,`Change`,Date from kKospi where Year(Date)>2000 and Year(Date)<2015 order by Date asc"""
+df = pd.read_sql(sql, conn)
+def features(df):
+    df = df.copy()
+    df['MA20'] = df['Close'].rolling(window=20).mean()
+    df['MA20_slope']=df['MA20'].pct_change()
+    df['Disparity'] = (df['Close'] - df['MA20']) / df['MA20']
+    df['Volatility'] = df['Change'].rolling(window=20).std()
+    vol_v1 = df['Volume'].shift(2)
+    vol_v2 = df['Volume'].shift(1)
+    df['Vol_Change'] = np.where(vol_v1==0,0,(vol_v1 - vol_v2) / vol_v1)
+    denominator=df["High"]-df["Low"]
+    df['Position']=np.where(denominator==0,0, (df["Close"]-df["Low"])/(denominator))
+    return df
+df_processed=features(df)
+df_processed.dropna(inplace=True)
+feature_cols = ['MA20_slope', 'Disparity', 'Volatility', 'Vol_Change', 'Position']
+X = df_processed[feature_cols]
+model = GaussianHMM(n_components=3, verbose=True, n_iter=100, random_state= 108).fit(X)
